31deb6b724ccd0e6c6f181d15fceaf048893002d
Krypton <manish.ecew4@gmail.com>
2021-03-14 13:44:02 +0530

    Product Category Page Done

--- application/views/frontent/category.php
+++ application/views/frontent/category.php
@@ -1,1 +1,137 @@
-<h1>Hello</h1>
+<section class="productsContainer dfrw mt-5">
+    <a class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a1.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </a>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a2.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a3.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a4.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a5.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a6.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a7.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a1.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a2.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a3.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a4.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a5.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a6.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a7.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+    <div class="productCard card">
+        <div class="productImage">
+            <img class="img_responsive" src="<?= base_url() ?>assets/frontent/images/a1.jpg">
+        </div>
+        <div class="productText text-left">
+            <h7 class="productCategory">Printed Dress</h7>
+            <p class="productColor">Blue , Golden</p>
+        </div>
+    </div>
+</section>
--- application/views/frontent/productsDetails.php
+++ application/views/frontent/productsDetails.php
@@ -1,3 +1,3 @@
 <section class="mainBody">
-
+    
 </section>
