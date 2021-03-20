c7322b38b84619dbe89b9dc5b1425f8f26dfdfb0
Krypton <manish.ecew4@gmail.com>
2021-03-21 01:16:13 +0530

    header

--- application/views/frontent/includes/header.php
+++ application/views/frontent/includes/header.php
@@ -16,7 +16,10 @@
 	<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
 	<!-- StyleSheet -->
-
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/bootstrap.css">
 	<!-- Magnific Popup -->
@@ -43,6 +46,7 @@
 	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/reset.css">
 	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/style.css">
 	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/responsive.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/header.css">
 
 
 
@@ -65,126 +69,78 @@
 	<!-- Header -->
 	<header class="header shop">
 		<!-- Topbar -->
-		<!-- <div class="topbar">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-4 col-md-12 col-12">
-						
-						<div class="top-left">
-							<ul class="list-main">
-								<li><i class="ti-headphone-alt"></i> +91 000 000 0000</li>
-								<li><i class="ti-email"></i> example@example.com</li>
-							</ul>
-						</div>
-						
-					</div>
-					<div class="col-lg-8 col-md-12 col-12">
-						
-						<div class="right-content">
-							<ul class="list-main">
-								<li><i class="ti-location-pin"></i> Store location</li>
-								<li><i class="ti-alarm-clock"></i> <a href="#">Daily deal</a></li>
-								<li><i class="ti-user"></i> <a href="#">My account</a></li>
-								<li><i class="ti-power-off"></i><a href="#">Login</a></li>
-							</ul>
-						</div>
-						
-					</div>
-				</div>
-			</div>
-		</div> -->
-		<!-- End Topbar -->
-		<div class="middle-inner">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-2 col-md-2 col-12">
-						<!-- Logo -->
-						<div class="logo">
-							<a href="#">
-								<img src="<?= base_url(); ?>assets/frontent/images/logoD.png" alt="logo">
-								<!-- <h3>Trenta</h3> -->
 
-							</a>
+		<!-- End Topbar -->
+		<div class="middle-inner" id="navbar">
+			<div class="container d-flex flex-row jcsb">
+
+				<div class="left_menu d-flex jcse">
+					<a href="">Home</a>
+					<a href="">About Us</a>
+				</div>
+				<!-- Logo -->
+				<div class="logo">
+					<a href="#">
+						<img src="<?= base_url(); ?>assets/frontent/images/logoD.png" alt="logo">
+					</a>
+				</div>
+				<div class="d-flex flex-row jcsb right_menu">
+					<div class="input-group linearGrad hfc mw-50">
+						<input type="text" class="form-control" placeholder="Search Here" aria-label="Search Here" aria-describedby="basic-addon2">
+						<div class="input-group-append">
+							<span class="input-group-text linearGrad" id="basic-addon2"><i class="fa fa-search" aria-hidden="true"></i></span>
 						</div>
-						<!--/ End Logo -->
+					</div>
+
+					<!-- Header Icon -->
+					<div class="right-bar hfc">
 						<!-- Search Form -->
-						<div class="search-top">
-							<div class="top-search"><a href="#0"><i class="ti-search"></i></a></div>
-							<!-- Search Form -->
-							<div class="search-top">
-								<form class="search-form">
-									<input type="text" placeholder="Search here..." name="search">
-									<button value="search" type="submit"><i class="ti-search"></i></button>
-								</form>
-							</div>
-							<!--/ End Search Form -->
+						<div class="sinlge-bar">
+							<a href="#" class="single-icon"><i class="fa yellow fa-heart-o" aria-hidden="true"></i></a>
 						</div>
-						<!--/ End Search Form -->
-						<div class="mobile-nav"></div>
-					</div>
-					<div class="col-lg-8 col-md-7 col-12">
-						<div class="search-bar-top">
-							<div class="search-bar">
-								<select>
-									<option selected="selected">All Category</option>
-									<option>watch</option>
-									<option>mobile</option>
-									<option>kid’s item</option>
-								</select>
-								<form>
-									<input name="search" placeholder="Search Products Here....." type="search">
-									<button class="btnn"><i class="ti-search"></i></button>
-								</form>
-							</div>
+						<div class="sinlge-bar">
+							<a href="#" class="single-icon"><i class="fa yellow fa-user-circle-o" aria-hidden="true"></i></a>
 						</div>
-					</div>
-					<div class="col-lg-2 col-md-3 col-12">
-						<div class="right-bar">
-							<!-- Search Form -->
-							<div class="sinlge-bar">
-								<a href="#" class="single-icon"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-							</div>
-							<div class="sinlge-bar">
-								<a href="#" class="single-icon"><i class="fa fa-user-circle-o" aria-hidden="true"></i></a>
-							</div>
-							<div class="sinlge-bar shopping">
-								<a href="#" class="single-icon"><i class="ti-bag"></i> <span class="total-count">2</span></a>
-								<!-- Shopping Item -->
-								<div class="shopping-item">
-									<div class="dropdown-cart-header">
-										<span>2 Items</span>
-										<a href="#">View Cart</a>
-									</div>
-									<ul class="shopping-list">
-										<li>
-											<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
-											<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
-											<h4><a href="#">Woman Ring</a></h4>
-											<p class="quantity">1x - <span class="amount">$99.00</span></p>
-										</li>
-										<li>
-											<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
-											<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
-											<h4><a href="#">Woman Necklace</a></h4>
-											<p class="quantity">1x - <span class="amount">$35.00</span></p>
-										</li>
-									</ul>
-									<div class="bottom">
-										<div class="total">
-											<span>Total</span>
-											<span class="total-amount">$134.00</span>
-										</div>
-										<a href="#" class="btn animate">Checkout</a>
-									</div>
+						<div class="sinlge-bar shopping">
+							<a href="#" class="single-icon"><i class="ti-bag yellow"></i> <span class="total-count">2</span></a>
+							<!-- Shopping Item -->
+							<div class="shopping-item">
+								<div class="dropdown-cart-header">
+									<span>2 Items</span>
+									<a href="#">View Cart</a>
 								</div>
-								<!--/ End Shopping Item -->
+								<ul class="shopping-list">
+									<li>
+										<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
+										<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
+										<h4><a href="#">Woman Ring</a></h4>
+										<p class="quantity">1x - <span class="amount">$99.00</span></p>
+									</li>
+									<li>
+										<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
+										<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
+										<h4><a href="#">Woman Necklace</a></h4>
+										<p class="quantity">1x - <span class="amount">$35.00</span></p>
+									</li>
+								</ul>
+								<div class="bottom">
+									<div class="total">
+										<span>Total</span>
+										<span class="total-amount">$134.00</span>
+									</div>
+									<a href="#" class="btn animate">Checkout</a>
+								</div>
 							</div>
+							<!--/ End Shopping Item -->
 						</div>
 					</div>
+
 				</div>
 			</div>
 		</div>
 		<!-- Header Inner -->
+
+
 		<div class="header-inner">
 			<div class="container">
 				<div class="cat-nav-head">
@@ -266,21 +222,6 @@
 											<ul class="nav main-menu menu navbar-nav">
 												<li class="active"><a href="#">Home</a></li>
 												<li><a href="#">Product</a></li>
-												<!-- <li><a href="#">Service</a></li> -->
-												<!-- <li><a href="#">Shop<i class="ti-angle-down"></i><span class="new">New</span></a>
-														<ul class="dropdown">
-															<li><a href="shop-grid.html">Shop Grid</a></li>
-															<li><a href="cart.html">Cart</a></li>
-															<li><a href="checkout.html">Checkout</a></li>
-														</ul>
-													</li>
-													<li><a href="#">Pages</a></li>									
-													<li><a href="#">Blog<i class="ti-angle-down"></i></a>
-														<ul class="dropdown">
-															<li><a href="blog-single-sidebar.html">Blog Single Sidebar</a></li>
-														</ul>
-													</li>
- -->
 												<li><a href="#">Contact Us</a></li>
 											</ul>
 										</div>
@@ -296,3 +237,20 @@
 		<!--/ End Header Inner -->
 	</header>
 	<!--/ End Header -->
+
+	<script>
+		window.onscroll = function() {
+			myFunction()
+		};
+
+		var navbar = document.getElementById("navbar");
+		var sticki = navbar.offsetTop;
+
+		function myFunction() {
+			if (window.pageYOffset >= sticki) {
+				navbar.classList.add("sticki")
+			} else {
+				navbar.classList.remove("sticki");
+			}
+		}
+	</script>
