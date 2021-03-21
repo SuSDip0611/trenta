22f60347b3e8e5b0a0af1e5b65e6d2a4ad85b1bb
Krypton <manish.ecew4@gmail.com>
2021-03-21 16:46:53 +0530

    Design improve

---
+++ application/views/frontent/includes/header_new.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<!-- Meta Tag -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name='copyright' content=''>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Title Tag  -->
+	<title>Treanta</title>
+	<!-- Favicon -->
+	<!-- <link rel="icon" type="image/png" href="<?= base_url(); ?>assets/frontent/images/favicon.png"> -->
+	<!-- Web Font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+
+	<!-- StyleSheet -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/bootstrap.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/magnific-popup.min.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/font-awesome.css">
+	<!-- Fancybox -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/jquery.fancybox.min.css">
+	<!-- Themify Icons -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/themify-icons.css">
+	<!-- Nice Select CSS -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/niceselect.css">
+	<!-- Animate CSS -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/animate.css">
+	<!-- Flex Slider CSS -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/flex-slider.min.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/owl-carousel.css">
+	<!-- Slicknav -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/slicknav.min.css">
+
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/lightslider.css">
+	<!-- Eshop StyleSheet -->
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/reset.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/style.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/responsive.css">
+	<link rel="stylesheet" href="<?= base_url(); ?>assets/frontent/css/header.css">
+
+
+
+</head>
+
+<body class="js">
+
+	<!-- Preloader -->
+	<div class="preloader">
+		<div class="preloader-inner">
+			<div class="preloader-icon">
+				<img src="<?= base_url(); ?>assets/frontent/images/logoD.png" alt="logo" class="img_responsive">
+			</div>
+		</div>
+	</div>
+	<!-- End Preloader -->
+
+
+	<!-- Header -->
+	<header class="header shop">
+		<!-- Topbar -->
+
+		<!-- End Topbar -->
+		<div class="middle-inner" id="navbar">
+			<div class="container d-flex flex_responsive jcsb ">
+				<div class="d-flex flex_responsive aismc">
+					<!-- Logo -->
+					<div class="logo scroll_d_block">
+						<a href="#" class="logo_wrap">
+							<img src="<?= base_url(); ?>assets/frontent/images/logoD.png" alt="logo" class="img_responsive">
+						</a>
+					</div>
+
+					<div class="left_menu aismc flex_responsive d-flex jcse sm_mt_10">
+						<a href="">Home</a>
+						<a href="" class="ml_md_3 sm_mt_10">About Us</a>
+					</div>
+				</div>
+				<div class="logo scroll_d_none sm_mt_10">
+					<a href="#" class="logo_wrap">
+						<img src="<?= base_url(); ?>assets/frontent/images/logoD.png" alt="logo" class="img_responsive">
+					</a>
+				</div>
+
+				<div class="d-flex flex_responsive jcsb right_menu aismc">
+					<div class="input-group linearGrad hfc mw-50">
+						<input type="text" class="form-control" placeholder="Search Here" aria-label="Search Here" aria-describedby="basic-addon2">
+						<div class="input-group-append">
+							<span class="input-group-text linearGrad" id="basic-addon2"><i class="fa fa-search" aria-hidden="true"></i></span>
+						</div>
+					</div>
+
+					<!-- Header Icon -->
+					<div class="right-bar hfc">
+						<!-- Search Form -->
+						<div class="sinlge-bar">
+							<a href="#" class="single-icon"><i class="fa yellow fa-heart-o" aria-hidden="true"></i></a>
+						</div>
+						<div class="sinlge-bar">
+							<a href="#" class="single-icon"><i class="fa yellow fa-user-circle-o" aria-hidden="true"></i></a>
+						</div>
+						<div class="sinlge-bar shopping">
+							<a href="#" class="single-icon"><i class="ti-bag yellow"></i> <span class="total-count">2</span></a>
+							<!-- Shopping Item -->
+							<div class="shopping-item">
+								<div class="dropdown-cart-header">
+									<span>2 Items</span>
+									<a href="#">View Cart</a>
+								</div>
+								<ul class="shopping-list">
+									<li>
+										<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
+										<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
+										<h4><a href="#">Woman Ring</a></h4>
+										<p class="quantity">1x - <span class="amount">$99.00</span></p>
+									</li>
+									<li>
+										<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
+										<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
+										<h4><a href="#">Woman Necklace</a></h4>
+										<p class="quantity">1x - <span class="amount">$35.00</span></p>
+									</li>
+								</ul>
+								<div class="bottom">
+									<div class="total">
+										<span>Total</span>
+										<span class="total-amount">$134.00</span>
+									</div>
+									<a href="#" class="btn animate">Checkout</a>
+								</div>
+							</div>
+							<!--/ End Shopping Item -->
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- Header Inner -->
+
+
+		<div class="header-inner">
+			<div class="container">
+				<div class="cat-nav-head">
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="all-category">
+								<h3 class="cat-heading"><i class="fa fa-bars" aria-hidden="true"></i>CATEGORIES</h3>
+								<ul class="main-category">
+									<li><a href="#">New Arrivals <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										<ul class="sub-category">
+											<li><a href="#">accessories</a></li>
+											<li><a href="#">best selling</a></li>
+											<li><a href="#">top 100 offer</a></li>
+											<li><a href="#">sunglass</a></li>
+											<li><a href="#">watch</a></li>
+											<li><a href="#">man’s product</a></li>
+											<li><a href="#">ladies</a></li>
+											<li><a href="#">westrn dress</a></li>
+											<li><a href="#">denim </a></li>
+										</ul>
+									</li>
+									<li class="main-mega"><a href="#">best selling <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										<ul class="mega-menu">
+											<li class="single-menu">
+												<a href="#" class="title-link">Shop Kid's</a>
+												<div class="image">
+													<img src="https://via.placeholder.com/225x155" alt="#">
+												</div>
+												<div class="inner-link">
+													<a href="#">Kids Toys</a>
+													<a href="#">Kids Travel Car</a>
+													<a href="#">Kids Color Shape</a>
+													<a href="#">Kids Tent</a>
+												</div>
+											</li>
+											<li class="single-menu">
+												<a href="#" class="title-link">Shop Men's</a>
+												<div class="image">
+													<img src="https://via.placeholder.com/225x155" alt="#">
+												</div>
+												<div class="inner-link">
+													<a href="#">Watch</a>
+													<a href="#">T-shirt</a>
+													<a href="#">Hoodies</a>
+													<a href="#">Formal Pant</a>
+												</div>
+											</li>
+											<li class="single-menu">
+												<a href="#" class="title-link">Shop Women's</a>
+												<div class="image">
+													<img src="https://via.placeholder.com/225x155" alt="#">
+												</div>
+												<div class="inner-link">
+													<a href="#">Ladies Shirt</a>
+													<a href="#">Ladies Frog</a>
+													<a href="#">Ladies Sun Glass</a>
+													<a href="#">Ladies Watch</a>
+												</div>
+											</li>
+										</ul>
+									</li>
+									<li><a href="#">accessories</a></li>
+									<li><a href="#">top 100 offer</a></li>
+									<li><a href="#">sunglass</a></li>
+									<li><a href="#">watch</a></li>
+									<li><a href="#">man’s product</a></li>
+									<li><a href="#">ladies</a></li>
+									<li><a href="#">westrn dress</a></li>
+									<li><a href="#">denim </a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-9 col-12">
+							<div class="menu-area">
+								<!-- Main Menu -->
+								<nav class="navbar navbar-expand-lg">
+									<div class="navbar-collapse">
+										<div class="nav-inner">
+											<ul class="nav main-menu menu navbar-nav">
+												<li class="active"><a href="#">Home</a></li>
+												<li><a href="#">Product</a></li>
+												<li><a href="#">Contact Us</a></li>
+											</ul>
+										</div>
+									</div>
+								</nav>
+								<!--/ End Main Menu -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Header Inner -->
+	</header>
+	<!--/ End Header -->
+	<!-- Mobile Header Start Here -->
+	<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light mobile_header">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<h4>Home</h4>
+		</div>
+	</nav> -->
+	<!-- Mobile Header End Here -->
+	<!-- Header Inner -->
+	<div class="header-inner">
+		<div class="container">
+			<div class="cat-nav-head">
+				<div class="row">
+					<div class="col-lg-3">
+						<div class="all-category">
+							<h3 class="cat-heading"><i class="fa fa-bars" aria-hidden="true"></i>CATEGORIES</h3>
+							<ul class="main-category">
+								<li><a href="#">New Arrivals <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+									<ul class="sub-category">
+										<li><a href="#">accessories</a></li>
+										<li><a href="#">best selling</a></li>
+										<li><a href="#">top 100 offer</a></li>
+										<li><a href="#">sunglass</a></li>
+										<li><a href="#">watch</a></li>
+										<li><a href="#">man’s product</a></li>
+										<li><a href="#">ladies</a></li>
+										<li><a href="#">westrn dress</a></li>
+										<li><a href="#">denim </a></li>
+									</ul>
+								</li>
+								<li class="main-mega"><a href="#">best selling <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+									<ul class="mega-menu">
+										<li class="single-menu">
+											<a href="#" class="title-link">Shop Kid's</a>
+											<div class="image">
+												<img src="https://via.placeholder.com/225x155" alt="#">
+											</div>
+											<div class="inner-link">
+												<a href="#">Kids Toys</a>
+												<a href="#">Kids Travel Car</a>
+												<a href="#">Kids Color Shape</a>
+												<a href="#">Kids Tent</a>
+											</div>
+										</li>
+										<li class="single-menu">
+											<a href="#" class="title-link">Shop Men's</a>
+											<div class="image">
+												<img src="https://via.placeholder.com/225x155" alt="#">
+											</div>
+											<div class="inner-link">
+												<a href="#">Watch</a>
+												<a href="#">T-shirt</a>
+												<a href="#">Hoodies</a>
+												<a href="#">Formal Pant</a>
+											</div>
+										</li>
+										<li class="single-menu">
+											<a href="#" class="title-link">Shop Women's</a>
+											<div class="image">
+												<img src="https://via.placeholder.com/225x155" alt="#">
+											</div>
+											<div class="inner-link">
+												<a href="#">Ladies Shirt</a>
+												<a href="#">Ladies Frog</a>
+												<a href="#">Ladies Sun Glass</a>
+												<a href="#">Ladies Watch</a>
+											</div>
+										</li>
+									</ul>
+								</li>
+								<li><a href="#">accessories</a></li>
+								<li><a href="#">top 100 offer</a></li>
+								<li><a href="#">sunglass</a></li>
+								<li><a href="#">watch</a></li>
+								<li><a href="#">man’s product</a></li>
+								<li><a href="#">ladies</a></li>
+								<li><a href="#">westrn dress</a></li>
+								<li><a href="#">denim </a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-9 col-12">
+						<div class="menu-area">
+							<!-- Main Menu -->
+							<nav class="navbar navbar-expand-lg">
+								<div class="navbar-collapse">
+									<div class="nav-inner">
+										<ul class="nav main-menu menu navbar-nav">
+											<li class="active"><a href="#">Home</a></li>
+											<li><a href="#">Product</a></li>
+
+											<li><a href="#">Contact Us</a></li>
+										</ul>
+									</div>
+								</div>
+							</nav>
+							<!--/ End Main Menu -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/ End Header Inner -->
+	<script>
+		window.onscroll = function() {
+			myFunction()
+		};
+
+		var navbar = document.getElementById("navbar");
+		var sticki = navbar.offsetTop;
+
+		function myFunction() {
+			if (window.pageYOffset >= sticki) {
+				navbar.classList.add("sticki")
+			} else {
+				navbar.classList.remove("sticki");
+			}
+		}
+	</script>
